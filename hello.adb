8604d278b4b410faa046139e2be9d09233d533ab
FDMZ17 <85776604+FDMZ17@users.noreply.github.com>
2021-11-14 09:13:05 +0700

    Hello world in 25 programing language

---
+++ hello.adb
@@ -0,0 +1,7 @@
+
+with Ada.Text_IO;
+use Ada.Text_IO;
+procedure AdaHelloworld is
+begin
+     Put_Line ("Hello world");
+end AdaHelloWorld;
---
+++ hello.php
@@ -0,0 +1,2 @@
+<?= 'Hello world' ?>
+<?php echo "Hello world"; ?>
